210380ffaab2a9b8bcf2b5746366a6a89a002fca
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2024-08-24 20:27:17 +0800

    8/24/2024

--- gym_system/gym_function/views.py
+++ gym_system/gym_function/views.py
@@ -78,6 +78,39 @@ def updateMember_views(request):
         return JsonResponse({'success': False, 'message': 'Invalid request method'})
 
 
+@csrf_exempt 
+def deleteMember_views(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            id_card = data.get('id_card')
+            connection = mysql.connector.connect(**config_server)
+            cursor = connection.cursor()
+
+
+            cursor.execute('SELECT COUNT(*) FROM gym_members WHERE id_card=%s', [id_card])
+            result = cursor.fetchone()
+
+            #if member is not exist say member does not exist
+            if result[0] == 0: 
+                return JsonResponse({'success': False, 'message': 'Member does not exist'})
+
+            
+            # Delete the member
+            cursor.execute('DELETE FROM gym_members WHERE id_card=%s', [id_card])
+
+            connection.commit()  # Commit the transaction
+
+            return JsonResponse({'success': True, 'message': 'Member deleted successfully'})
+        
+        except mysql.connector.Error as err:
+            return JsonResponse({'success': False, 'message': f'Database error: {err}'})
+    else:
+        return JsonResponse({'success': False, 'message': 'Invalid request method'})
+
+
+
+
 
 @csrf_exempt
 def registerClass_views(request):
